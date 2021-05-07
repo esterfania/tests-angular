76e89e27436ddd884ca3f9125cdb44519c0b1ec1
Esterfania <esterfania@MacBook-Pro-de-Esterfania.local>
2021-05-06 22:37:38 -0300

    feat: add photo-frame unit tests

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,3 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LikeWidgetComponent } from '../like-widget/like-widget.component';
 import { PhotoFrameComponent } from './photo-frame.component';
+import { PhotoFrameModule } from './photo-frame.module';
 
-describe(PhotoFrameComponent.name, () => {});
+describe(PhotoFrameComponent.name, () => {
+  let component: PhotoFrameComponent;
+  let componentElement: HTMLElement;
+  let fixture: ComponentFixture<PhotoFrameComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PhotoFrameModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PhotoFrameComponent);
+    component = fixture.componentInstance;
+    componentElement = fixture.debugElement.nativeElement;
+  });
+
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  });
+});
