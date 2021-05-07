12d4b1ab7cd57f97627ee1e1529af3ffc6edbd23
Esterfania <esterfania@MacBook-Pro-de-Esterfania.local>
2021-05-06 22:02:33 -0300

    add photo-frame component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
 import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/shared/components/photo-frame/photo-frame.component.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,17 +1,38 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
   styleUrls: ['./photo-frame.component.scss'],
 })
-export class PhotoFrameComponent {
+export class PhotoFrameComponent implements OnInit, OnDestroy {
   @Output() liked: EventEmitter<void> = new EventEmitter();
   @Input() description = '';
   @Input() src = '';
   @Input() likes = 0;
-
+  private debounceSubject = new Subject();
+  private unsubscribe = new Subject();
+  ngOnInit(): void {
+    this.debounceSubject
+      .asObservable()
+      .pipe(debounceTime(500))
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(() => this.liked.emit());
+  }
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
   like(): void {
-    this.liked.emit();
+    this.debounceSubject.next();
   }
 }
